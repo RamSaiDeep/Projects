be5e0b9d3091a931cf4e823a23ec780f540bb00b
Ram Sai Deep <ramasaideepv@gmail.com>
2026-06-03 19:25:21 +0530

    EDA some part done

---
+++ hello.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print('Welcome to Python programming.')
+print('This is a simple Python script.')
+print('You can run this script to see the output.')
+print('Python is a versatile language.')
+print('You can use it for web development, data analysis, and more.')
+print('Have fun learning Python!')
